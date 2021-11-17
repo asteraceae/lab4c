97e57220585f41ca4550a812f28e19f9cfe5b10e
asteraceae <dreyes5@luc.edu>
2021-11-16 19:23:45 -0600

    i'm special so my login details get to go in

--- flaskDemo/__init__.py
+++ flaskDemo/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://cfn:naiman@localhost/company'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://student:student@localhost/company'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
