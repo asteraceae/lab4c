2b9a20a25de808f2dc0d49692da7c33ac6a2dfde
asteraceae <dreyes5@luc.edu>
2021-11-22 22:04:47 -0600

    delete

--- flaskDemo/routes.py
+++ flaskDemo/routes.py
@@ -163,7 +163,7 @@ def delete_dept(dnumber):
 
 @app.route("/empl/new_assignment", methods=['GET', 'POST'])
 @login_required
-def new_empl_assign(pnumber):
+def new_empl_assign():
     form = EmplForm()
     if form.validate_on_submit():
         empl = Works_On(pno = pno, essn = form.emp_ssn.data, hours = form.hours.data)
@@ -176,7 +176,7 @@ def new_empl_assign(pnumber):
 
 @app.route("/empl/remove", methods=['GET', 'POST'])
 @login_required
-def remove_empl_assign(pnumber):
+def remove_empl_assign():
     form = removeEmplForm()
     if form.validate_on_submit():
         empl = Works_On.query.filter_by(essn = form.essn.data, pno = pno).first()
