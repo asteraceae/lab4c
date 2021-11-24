9a212ac3ee78e2dee2ef2e295271c04efbfc6864
asteraceae <dreyes5@luc.edu>
2021-11-23 18:17:51 -0600

    a

--- flaskDemo/routes.py
+++ flaskDemo/routes.py
@@ -4,7 +4,7 @@ from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from flaskDemo import app, db, bcrypt
 from flaskDemo.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, DeptForm,DeptUpdateForm, EmplForm, EmployeeUpdateForm, removeEmplForm
-import forms
+import forms.py
 from flaskDemo.models import User, Post,Department, Dependent, Dept_Locations, Employee, Project, Works_On
 from flask_login import login_user, current_user, logout_user, login_required
 from datetime import datetime
