321b4d6aa989346f5ab939f512cd8dca6d0cb8d3
asteraceae <dreyes5@luc.edu>
2021-11-23 22:58:16 -0600

    a

--- flaskDemo/forms.py
+++ flaskDemo/forms.py
@@ -193,5 +193,6 @@ class removeEmplForm(EmployeeUpdateForm):
         ssn = Employee.query.filter_by(fname = name).first()
         #now see if ssn exists in works on
         empl = Works_On.query.filter_by(essn = ssn.ssn, pno = pnumber_save).first()
+        print(empl)
         if not empl:
             raise ValidationError("That employee doesn't work in this project!")
